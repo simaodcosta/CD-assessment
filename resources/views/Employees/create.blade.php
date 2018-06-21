22484df0ccaaa316af8425dfec79003ce3665b76
Simão Costa <simaocosta@MacBook-Pro-de-Simao.local>
2018-06-21 05:37:34 +0100

    v0.3

--- resources/views/Employees/create.blade.php
+++ resources/views/Employees/create.blade.php
@@ -8,21 +8,21 @@
 
 @section('content')
   <div class="container">
-      <form method="post" action="{{url('employees')}}" enctype="multipart/form-data">
-        @csrf
-        <div class="row">
-          <div class="form-group col-md-4">
-            <label for="first_name">First Name:</label>
-            <input type="text" class="form-control" name="first_name">
+      @if($companies != null)
+        <form method="post" action="{{url('employees')}}" enctype="multipart/form-data">
+          @csrf
+          <div class="row">
+            <div class="form-group col-md-4">
+              <label for="first_name">First Name:</label>
+              <input type="text" class="form-control" name="first_name">
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="form-group col-md-4">
-            <label for="last_name">Last Name:</label>
-            <input type="text" class="form-control" name="last_name">
+          <div class="row">
+            <div class="form-group col-md-4">
+              <label for="last_name">Last Name:</label>
+              <input type="text" class="form-control" name="last_name">
+            </div>
           </div>
-        </div>        
-        @if($companies != null)
           <div class="row">
             <div class="form-group col-md-4">
               <label for="company_id">Company:</label>
@@ -33,24 +33,26 @@
               </select>
             </div>
           </div>
-        @endif
-        <div class="row">
-          <div class="form-group col-md-4">
-            <label for="phone">Phone Number:</label>
-            <input type="text" class="form-control" name="phone">
+          <div class="row">
+            <div class="form-group col-md-4">
+              <label for="phone">Phone Number:</label>
+              <input type="text" class="form-control" name="phone">
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="form-group col-md-4">
-            <label for="email">Email:</label>
-            <input type="text" class="form-control" name="email">
+          <div class="row">
+            <div class="form-group col-md-4">
+              <label for="email">Email:</label>
+              <input type="text" class="form-control" name="email">
+            </div>
           </div>
-        </div>
-        <div class="row">
-          <div class="form-group col-md-4" style="margin-top:10px">
-            <button type="submit" class="btn btn-success">Submit</button>
+          <div class="row">
+            <div class="form-group col-md-4" style="margin-top:10px">
+              <button type="submit" class="btn btn-success">Submit</button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      @else
+        <h2>You need to add a company first !</h2>
+      @endif
     </div>
 @stop
